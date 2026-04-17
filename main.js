148d4e002fbae8c9ac7a149847aa9defa4b0792c
RG GAMERS <rohit.gaikwad4312@gmail.com>
2026-04-17 22:23:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -166,7 +166,7 @@ const DEFAULT_GAMES = [
     category: 'Simulator',
     description: 'A flexible software sketchbook and a language for learning how to code within the context of the visual arts. Built on Java, highly efficient for data processing and visual synthesis.',
     size: '111 MB',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Processing_2021_logo.svg/512px-Processing_2021_logo.svg.png',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Processing_I3-3.5.4.png',
     downloadUrl: 'https://pixeldrain.com/u/4Bk9SWp6',
     host: 'pixeldrain',
     sysReq: {
